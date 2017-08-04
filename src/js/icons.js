f0ca294cd6f0e8f948270fd057f22cb221c53eb8
yambal <jy@yambal.net>
2017-08-04 17:00:40 +0900

    #1, #2, #3, #4, #5, #6, #7, #8, #9, #10, #11 対応

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,6 +1,6 @@
 var icons = [
-	{label:"Info", value:"&#59648;", color:""},
-	{label:"機能", value:"&#xe901;", color:""},
+	{label:"Info", value:"&#xe900;", color:""},
+	{label:"機能", value:"&#xe937;", color:""},
 	{label:"カレンダー", value:"&#xe902;", color:""},
 	{label:"ドキュメント", value:"&#xe903;", color:""},
 	{label:"チャット", value:"&#xe904;", color:""},
@@ -12,11 +12,20 @@ var icons = [
 	{label:"計算", value:"&#xe90a;", color:""},
 	{label:"削除", value:"&#xe90b;", color:""},
 	{label:"ストレージ", value:"&#xe90c;", color:""},
-	{label:"クレジットカード", value:"&#xe967;", color:""},
+	{label:"クレジットカード", value:"&#xe911;", color:""},
+	{label:"box", value:"&#xe91a;", color:"0,97,213"},
 	{label:"Dropbox", value:"&#xe91d;", color:"0,126,229"},
 	{label:"Google Analytics", value:"&#xe929;", color:"245,125,1"},
-	{label:"Google Doc", value:"&#xe92a;", color:""},
-	{label:"Slack", value:"&#xe946;", color:"120,212,182"},
+	{label:"Google Calender", value:"&#xe934;", color:"68,132,240"},
+	{label:"Google Cloud Print", value:"&#xe923;", color:"87,127,192"},
+	{label:"Google Drive", value:"&#xe92a;", color:"72,136,247"},
+	{label:"Google Groups", value:"&#xe935;", color:"65,105,255"},
+	{label:"Google Sheets", value:"&#xe912;", color:"16,157,68"},
+	{label:"Kintone", value:"&#xe914;", color:"252,184,0"},
+	{label:"NHK 番組表API", value:"&#xe936;", color:"28,98,158"},
+	{label:"salesforce", value:"&#xe917;", color:"16,154,215"},
 	{label:"PayPal", value:"&#xe960;", color:"0,112,186"},
-	{label:"Stripe", value:"&#xe966;", color:"62,138,198"}
+	{label:"Slack", value:"&#xe946;", color:"120,212,182"},
+	{label:"Stripe", value:"&#xe92c;", color:"62,138,198"},
+	{label:"法人番号 API", value:"&#xe919;", color:"219,5,32"}
 ];
