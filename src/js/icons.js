c426373682d93c02f00902efb008c73dfdbd7b5e
June YAMAMOTO <yamamoto@questetra.com>
2017-12-11 12:32:31 +0900

    歯車、雲、雲と稲妻、Questetra のアイコンを追加

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,10 +1,12 @@
 var icons = [
 	{label:"Info", value:"&#xe900;", color:""},
+	{label:"稲妻", value:"&#xe940;", color:""},
 	{label:"機能", value:"&#xe937;", color:""},
 	{label:"カレンダー", value:"&#xe902;", color:""},
 	{label:"ドキュメント", value:"&#xe903;", color:""},
 	{label:"チャット", value:"&#xe904;", color:""},
 	{label:"接続", value:"&#xe905;", color:""},
+	{label:"歯車", value:"&#xe93c;", color:""},
 	{label:"メッセージ", value:"&#xe906;", color:""},
 	{label:"フィルター", value:"&#xe907;", color:""},
 	{label:"加工", value:"&#xe908;", color:""},
@@ -27,9 +29,11 @@ var icons = [
 	{label:"NHK 番組表API", value:"&#xe936;", color:"28,98,158"},
 	{label:"salesforce", value:"&#xe917;", color:"16,154,215"},
 	{label:"PayPal", value:"&#xe960;", color:"0,112,186"},
+	{label:"Questetra", value:"&#xe93b;", color:"0,153,0"},
 	{label:"Slack", value:"&#xe946;", color:"120,212,182"},
 	{label:"Stripe", value:"&#xe92c;", color:"62,138,198"},
 	{label:"typetalk", value:"&#xe938;", color:"252,99,71"},
 	{label:"Twilio", value:"&#xe94e;", color:"242,47,70"},
-	{label:"法人番号 API", value:"&#xe919;", color:"219,5,32"}
+	{label:"法人番号 API", value:"&#xe919;", color:"219,5,32"},
+	{label:"その他のクラウド連携", value:"&#xe93d;", color:"0,153,0"},
 ];
